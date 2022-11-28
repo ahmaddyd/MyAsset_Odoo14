da154327cb5309808c728d63379b3f5e2c4edb82
ahmaddyd <ahmdyulian.86@gmail.com>
2022-11-29 01:53:18 +0700

    Change category from manifest.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "testingNewFolder",
+    'name': "My Asset",
 
     'summary': """
        Test Coding Odoo MNC Odoo14""",
@@ -15,7 +15,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Services/Project',
+    'category': 'Productivity',
     'version': '0.1',
     'application': True,
 
